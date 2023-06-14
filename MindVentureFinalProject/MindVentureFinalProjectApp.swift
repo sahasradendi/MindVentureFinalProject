af0e316daf668586c1cdc7a18e47c933d31b4131
Sahasra Dendi <sahasra.dendi2007@gmail.com>
2023-06-14 13:17:23 -0500

    Initial Commit

---
+++ MindVentureFinalProject/MindVentureFinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindVentureFinalProjectApp.swift
+//  MindVentureFinalProject
+//
+//  Created by Kirankumar dendi on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct MindVentureFinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
